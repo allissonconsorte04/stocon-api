c907bb6ae8c1692faee975554224333ceb2eccd7
Allisson Consorte <allissonconsorte@outlook.com>
2024-09-12 14:46:01 -0300

    fix: allow any connection cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'postgresql://postgres:aPOlTXyVAfKHKtKKRYnuPcZyqhnHPXtW@monorail.proxy.rlwy.net:31294/railway'
+    origins '*'
 
     resource '/v1/*',
       headers: :any,
