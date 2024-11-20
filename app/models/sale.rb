a1a6268742efff221111cd35bdb82a55321535a9
Allisson Consorte <allissonconsorte@outlook.com>
2024-11-20 18:10:37 -0300

    fix: fixed the total_amount on sales route

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -4,11 +4,11 @@ class Sale < ApplicationRecord
   has_many :products, through: :sale_items # Produtos vendidos nesta venda
 
   # Calcula o valor total da venda
-  before_save :calculate_total_amount
+  after_commit :calculate_total_amount
 
   private
 
   def calculate_total_amount
-    self.total_amount = sale_items.sum(&:total_price) # Calcula a soma dos preços totais dos itens
+    update_column(:total_amount, sale_items.sum(&:total_price)) # Atualiza o valor diretamente no banco
   end
 end
